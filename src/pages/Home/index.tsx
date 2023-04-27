1063704df3a4825d912a337412cd3a4358548820
Riquelme Maia Rodrigues <riquelmemaia2@gmail.com>
2023-04-27 00:25:23 -0300

    installed dependencies and created appbar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import ResponsiveAppBar from "../../components/AppBar";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <ResponsiveAppBar />
   );
 }
 
